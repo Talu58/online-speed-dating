c1a9fc583a03e8c561ea61f665bceb0b6c049dd9
Eric Lau <ericlau@Corinnas-MacBook-Air.local>
2016-12-07 21:06:14 -0800

    misc issues resolved

--- App/Client/app.js
+++ App/Client/app.js
@@ -5,13 +5,9 @@ import AppTEST from './appController.js';
 import login from './Views/loginController.js';
 import video from './Views/videoController.js';
 import signup from './Views/signupController.js';
-<<<<<<< HEAD
-
-=======
 import profile from './Views/profileController.js';
 import profileCreate from './Views/profileCreationController.js';
 import Vuex from 'vuex';
->>>>>>> working on updating user info in database
 Vue.use(VueResource);
 Vue.use(VueRouter);
 
