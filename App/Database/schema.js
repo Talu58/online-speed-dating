dc145a3a547c61653091d14237d50bba6ae2f164
zeninspire <zeninspire@gmail.com>
2016-12-14 15:51:10 -0800

    user schema updated

--- App/Database/schema.js
+++ App/Database/schema.js
@@ -6,41 +6,40 @@ var mongoose = require('mongoose');
 
 /////// End of Encryption Middleware ///////
 
-var userSchema = mongoose.Schema({
-  username: {type: String, required: true, index: { unique: true } },
-  password: String,
-  userinfo: {type: String, default: 'User did not provide info'},
-  name: {type: String, default: 'please fill out'},
-  age: {type: Number, default: '0'},
-  location: {type: String, default: 'please fill out'},
-  profileImg: {type: String, default: 'https://www.svgimages.com/svg-image/s4/question-mark-face-256x256.png'},
-  gender: {type: String, default: 'please fill out'},
-  admin: {type: Boolean, default: false},
-  events: {type: Array, default: []},
-  callList: {type: Array, default: []},
-  matches: {type: Array, default: []}
-});
-
-
-/////// Start of Encryption ///////
-
 // var userSchema = mongoose.Schema({
 //   username: {type: String, required: true, index: { unique: true } },
-//   // email: {type: String, required: true},
-//   password: {type: String, required: true},
-//   // salt: String,
-//   userinfo: {type: String, required: true, default: ''},
-//   name: {type: String, required: true, default: ''},
-//   profileImg: {type: String, default: 'http://www.returnofkings.com/wp-content/uploads/2014/04/online-dating-header2.jpg'},
+//   password: String,
+//   userinfo: {type: String, default: 'User did not provide info'},
+//   name: {type: String, default: 'please fill out'},
 //   age: {type: Number, default: '0'},
-//   gender: {type: String, required: true, default: ''},
-//   location: {type: String, required: true, default: ''},
-//   interestedIn: {type: String, required: true, default: ''},
+//   location: {type: String, default: 'please fill out'},
+//   profileImg: {type: String, default: 'https://www.svgimages.com/svg-image/s4/question-mark-face-256x256.png'},
+//   gender: {type: String, default: 'please fill out'},
 //   admin: {type: Boolean, default: false},
 //   events: {type: Array, default: []},
 //   callList: {type: Array, default: []},
 //   matches: {type: Array, default: []}
 // });
+
+
+/////// Start of Encryption ///////
+
+var userSchema = mongoose.Schema({
+  username: {type: String, required: true, index: { unique: true } },
+  password: {type: String, required: true},
+  // salt: String,
+  profileImg: {type: String, default: 'http://www.returnofkings.com/wp-content/uploads/2014/04/online-dating-header2.jpg'},
+  name: {type: String, default: ''},
+  age: {type: Number, default: '21'},
+  gender: {type: String, default: ''},
+  interestedIn: {type: String, default: ''},
+  location: {type: String, default: '94105'},
+  userinfo: {type: String, default: ''},
+  admin: {type: Boolean, default: false},
+  events: {type: Array, default: []},
+  callList: {type: Array, default: []},
+  matches: {type: Array, default: []}
+});
 
 // userSchema.methods.checkPassword = function(candidatePassword, cb) {
 //   bcrypt.compare(candidatePassword, this.password, function(err, isMatch){
