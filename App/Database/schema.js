912b793756d667a17993c297b169a90ab9b119ef
zeninspire <zeninspire@gmail.com>
2016-12-14 15:06:51 -0800

    reverting back to test functionality

--- App/Database/schema.js
+++ App/Database/schema.js
@@ -1,56 +1,78 @@
 var mongoose = require('mongoose');
+/////// Start of Encryption Middleware ///////
+
 // var bcrypt = require('bcrypt-nodejs');
 // var SALT_WORK_FACTOR = 10;
 
+/////// End of Encryption Middleware ///////
+
 var userSchema = mongoose.Schema({
   username: {type: String, required: true, index: { unique: true } },
-  // email: {type: String, required: true},
-  password: {type: String, required: true},
-  // salt: String,
-  userinfo: {type: String, required: true, default: ''},
-  name: {type: String, required: true, default: ''},
+  password: String,
+  userinfo: {type: String, default: 'User did not provide info'},
+  name: {type: String, default: 'please fill out'},
   age: {type: Number, default: '0'},
-  location: {type: String, required: true, default: ''},
-  profileImg: {type: String, default: 'http://www.returnofkings.com/wp-content/uploads/2014/04/online-dating-header2.jpg'},
-  gender: {type: String, required: true, default: ''},
+  location: {type: String, default: 'please fill out'},
+  profileImg: {type: String, default: 'https://www.svgimages.com/svg-image/s4/question-mark-face-256x256.png'},
+  gender: {type: String, default: 'please fill out'},
   admin: {type: Boolean, default: false},
   events: {type: Array, default: []},
   callList: {type: Array, default: []},
   matches: {type: Array, default: []}
 });
 
+
 /////// Start of Encryption ///////
-userSchema.methods.checkPassword = function(candidatePassword, cb) {
-  bcrypt.compare(candidatePassword, this.password, function(err, isMatch){
 
-    if (err) {
-      return cb(err);
-    }
-    cb (null, isMatch)
-  })
-};
+// var userSchema = mongoose.Schema({
+//   username: {type: String, required: true, index: { unique: true } },
+//   // email: {type: String, required: true},
+//   password: {type: String, required: true},
+//   // salt: String,
+//   userinfo: {type: String, required: true, default: ''},
+//   name: {type: String, required: true, default: ''},
+//   profileImg: {type: String, default: 'http://www.returnofkings.com/wp-content/uploads/2014/04/online-dating-header2.jpg'},
+//   age: {type: Number, default: '0'},
+//   gender: {type: String, required: true, default: ''},
+//   location: {type: String, required: true, default: ''},
+//   interestedIn: {type: String, required: true, default: ''},
+//   admin: {type: Boolean, default: false},
+//   events: {type: Array, default: []},
+//   callList: {type: Array, default: []},
+//   matches: {type: Array, default: []}
+// });
 
-userSchema.pre('save', function(next){
-  var user = this;
+// userSchema.methods.checkPassword = function(candidatePassword, cb) {
+//   bcrypt.compare(candidatePassword, this.password, function(err, isMatch){
 
-  if (!user.isModified('password')) {
-    return next();
-  }
+//     if (err) {
+//       return cb(err);
+//     }
+//     cb (null, isMatch)
+//   })
+// };
 
-  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-    if (err) {
-      return next(err);
-    }
-    bcrypt.hash(user.password, salt, null, function(err, hash){
-      if (err) {
-        return next(err);
-      }
-      user.password = hash;
-      user.salt = salt;
-      next();
-    });
-  });
-})
+// userSchema.pre('save', function(next){
+//   var user = this;
+
+//   if (!user.isModified('password')) {
+//     return next();
+//   }
+
+//   bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+//     if (err) {
+//       return next(err);
+//     }
+//     bcrypt.hash(user.password, salt, null, function(err, hash){
+//       if (err) {
+//         return next(err);
+//       }
+//       user.password = hash;
+//       user.salt = salt;
+//       next();
+//     });
+//   });
+// })
 
 /////// End of Encryption ///////
 
@@ -58,8 +80,8 @@ userSchema.pre('save', function(next){
 var eventSchema = mongoose.Schema({
   date: {type: Date, required: true },
   usernames: {type: Array, default: []},
-  eventType: {type: String, default: 'please fill out'},
-  eventName: {type: String, required: true, index: { unique: true }, default: 'please fill out'},
+  eventType: {type: String, default: ''},
+  eventName: {type: String, required: true, index: { unique: true }, default: ''},
   eventCallDuration: {type: Number, default: 300000},
 });
 
