1dfbddb079c0dbba12dadcf56d59187b15ead8f0
zeninspire <zeninspire@gmail.com>
2016-12-17 12:07:46 -0800

    got rid of undefined in landing page

--- App/Client/Components/landingPageController.js
+++ App/Client/Components/landingPageController.js
@@ -3,8 +3,8 @@ import temp from '../Templates/landingPageTemplate.vue';
 var app = {
   template: temp.template,
   data: function() {
-    return { 
-      msg: 'Welcome To Bash To The Dating! ' + this.$store.state.user.username,
+    return {
+      msg: 'Welcome To Bash To The Dating! ' + (this.$store.state.user.username || ''),
       background: '../Images/landing-bg.jpg'
     };
   },
@@ -14,22 +14,22 @@ var app = {
       this.$http.post('/event/start', {
         eventId: 1234
       })
-      .then((res) => { 
+      .then((res) => {
         var body = res.body;
         this.$store.commit('setUser', body);
       })
-      .catch((err) => console.error(err)); 
+      .catch((err) => console.error(err));
     },
-    
+
     TESTpublash: function() {
       this.$http.post('/event/setup', {
         eventName: 'Menergy'
       })
-      .then((res) => { 
+      .then((res) => {
         var body = res.body;
         this.$store.commit('setUser', body);
       })
-      .catch((err) => console.error(err)); 
+      .catch((err) => console.error(err));
     },
   },
 };
